3b96100dd400e2487e63702694d47c19c1b46a70
ac8007 <alexcosta8007@gmail.com>
2024-05-14 12:37:33 -0300

    3° commit - Portifolio atual cedupflix

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+//import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+function App() {  
+  return (
+    <>
+    <Header/>
+      <h1>OLÁ MUNDO!!!</h1>
+      <p>olá Mundão! Estou aprendendo React JS!</p>
+      <Footer/>     
+    </>
+  )
+}
+
+export default App;
